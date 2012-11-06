10ec9ccd0b3b529cd4bb66fed6b5d559b922a9e1
Theo Spears <theo@theos.me.uk>
2012-11-06 18:15:25 -0500

    Rename protected methods to match convention

--- experiments/utils.py
+++ experiments/utils.py
@@ -28,35 +28,35 @@ class WebUser(object):
         self.user = request.user
         self.session = request.session
 
-    def is_anonymous(self):
+    def _is_anonymous(self):
         return self.user.is_anonymous()
 
-    def get_registered_user(self):
-        if self.is_anonymous():
+    def _get_registered_user(self):
+        if self._is_anonymous():
             return None
         return self.user
 
-    def is_bot(self):
+    def _is_bot(self):
         return bool(BOT_REGEX.search(self.request.META.get("HTTP_USER_AGENT","")))
 
-    def is_verified_human(self):
+    def _is_verified_human(self):
         if getattr(settings, 'EXPERIMENTS_VERIFY_HUMAN', True):
             return self.session.get('experiments_verified_human', False)
         else:
             return True
 
-    def participant_identifier(self):
-        if not self.is_anonymous():
+    def _participant_identifier(self):
+        if not self._is_anonymous():
             return 'user:%d' % (self.user.pk,)
         else:
             if not self.session.session_key:
                 self.session.save() # Force session key
             return 'session:%s' % (self.session.session_key,)
 
-    def increment_participant_count(self, experiment, alternative_name):
+    def _increment_participant_count(self, experiment, alternative_name):
         # Increment experiment_name:alternative:participant counter
         counter_key = PARTICIPANT_KEY % (experiment.name, alternative_name)
-        count = counters.increment(counter_key, self.participant_identifier())
+        count = counters.increment(counter_key, self._participant_identifier())
 
         signals.experiment_incr_participant.send(
             sender=self,
@@ -67,10 +67,10 @@ class WebUser(object):
             participants=count,
         )  
 
-    def increment_goal_count(self, experiment, alternative_name, goal_name):
+    def _increment_goal_count(self, experiment, alternative_name, goal_name):
         # Increment experiment_name:alternative:participant counter
         counter_key = GOAL_KEY % (experiment.name, alternative_name, goal_name)
-        count = counters.increment(counter_key, self.participant_identifier())
+        count = counters.increment(counter_key, self._participant_identifier())
 
         signals.goal_hit.send(
             sender=self,
@@ -99,17 +99,17 @@ class WebUser(object):
         for experiment_name, data in enrollments.items():
             alternative, goals = data
             # Increment experiment_name:alternative:participant counter
-            self.increment_participant_count(experiment_manager[experiment_name], alternative)
+            self._increment_participant_count(experiment_manager[experiment_name], alternative)
 
 
     def get_enrollment(self, experiment):
-        if self.is_bot():
+        if self._is_bot():
             # Bot/Spider, so send back control group
             return CONTROL_GROUP
-        if not self.is_anonymous():
+        if not self._is_anonymous():
             # Registered User
             try:
-                return Enrollment.objects.get(user=self.get_registered_user(), experiment=experiment).alternative
+                return Enrollment.objects.get(user=self._get_registered_user(), experiment=experiment).alternative
             except Enrollment.DoesNotExist:
                 return None
         else:
@@ -121,13 +121,13 @@ class WebUser(object):
             return None
 
     def set_enrollment(self, experiment, alternative):
-        if self.is_bot():
+        if self._is_bot():
             # Bot/Spider, so don't enroll
             return
-        if not self.is_anonymous():
+        if not self._is_anonymous():
             # Registered User
             try:
-                enrollment, _ = Enrollment.objects.get_or_create(user=self.get_registered_user(), experiment=experiment, defaults={'alternative':alternative})
+                enrollment, _ = Enrollment.objects.get_or_create(user=self._get_registered_user(), experiment=experiment, defaults={'alternative':alternative})
             except IntegrityError, exc:
                 # Already registered (db race condition under high load)
                 return
@@ -137,16 +137,16 @@ class WebUser(object):
                 enrollment.save()
 
             # Increment experiment_name:alternative:participant counter
-            self.increment_participant_count(experiment, alternative)
+            self._increment_participant_count(experiment, alternative)
         else:
             # Not registered use Sessions
             enrollments = self.session.get('experiments_enrollments', {})
             enrollments[experiment.name] = (alternative, [])
             self.session['experiments_enrollments'] = enrollments
             # Only Increment participant count for verified users
-            if self.is_verified_human():
+            if self._is_verified_human():
                 # Increment experiment_name:alternative:participant counter
-                self.increment_participant_count(experiment, alternative)
+                self._increment_participant_count(experiment, alternative)
 
         signals.experiment_user_added.send(
             sender=self,
@@ -156,7 +156,7 @@ class WebUser(object):
             alternative=alternative,
         )    
 
-    def should_increment(self, experiment, goals, goal_name):
+    def _should_increment(self, experiment, goals, goal_name):
         # Increments goal if enabled+gargoyle switch conditions met or just enabled
         # Goal uniquely incremented for all enrollments at once
 
@@ -176,27 +176,27 @@ class WebUser(object):
     # Checks if the goal should be incremented
     def record_goal(self, goal_name):
         # Bots don't register goals
-        if self.is_bot():
+        if self._is_bot():
             return
         # If the user is registered
-        if not self.is_anonymous():
-            enrollments = Enrollment.objects.filter(user=self.get_registered_user())
+        if not self._is_anonymous():
+            enrollments = Enrollment.objects.filter(user=self._get_registered_user())
             if not enrollments:
                 return
             for enrollment in enrollments: # Looks up by PK so no point caching.
-                if self.should_increment(enrollment.experiment, enrollment.goals, goal_name):
-                    self.increment_goal_count(enrollment.experiment, enrollment.alternative, goal_name)
+                if self._should_increment(enrollment.experiment, enrollment.goals, goal_name):
+                    self._increment_goal_count(enrollment.experiment, enrollment.alternative, goal_name)
                     enrollment.goals.append(goal_name)
                     enrollment.save()
             return
         # If confirmed human
-        if self.is_verified_human():
+        if self._is_verified_human():
             enrollments = self.session.get('experiments_enrollments', None)
             if not enrollments:
                 return
             for experiment_name, (alternative, goals) in enrollments.items():
-                if self.should_increment(experiment_manager[experiment_name], goals, goal_name):
-                    self.increment_goal_count(experiment_manager[experiment_name], alternative, goal_name)
+                if self._should_increment(experiment_manager[experiment_name], goals, goal_name):
+                    self._increment_goal_count(experiment_manager[experiment_name], alternative, goal_name)
                     goals.append(goal_name)
 
             self.session['experiments_enrollments'] = enrollments
