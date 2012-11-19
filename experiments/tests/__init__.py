809052db150bd2e1f4102c19a17aa5292f1958cf
Theo Spears <theo@theos.me.uk>
2012-11-19 15:55:17 -0500

    Split tests out into multiple files

---
+++ experiments/tests/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from .stats import StatsTestCase
+from .mannwhitney import MannWhitneyTestCase
+from .counter import CounterTestCase
+from .webuser import WebUserAnonymousTestCase, WebUserAuthenticatedTestCase, BotTestCase
