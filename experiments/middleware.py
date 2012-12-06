a4003fa54afacbc98df10853e9011ae6c029db3e
Florian <moga.flo@gmail.com>
2012-12-06 13:36:59 +0200

    Bind cookies in the capture phase of event handling so that they'll be set in the bubbling phase for any outgoing requests. Add support for multiple goals in cookie.

--- experiments/middleware.py
+++ experiments/middleware.py
@@ -4,6 +4,7 @@ class ExperimentsMiddleware(object):
     def process_response(self, request, response):
         experiments_goal = request.COOKIES.get('experiments_goal', None)
         if experiments_goal:
-            record_goal(experiments_goal, request)
+            for goal in experiments_goal.split('%20'): # multiple goals separated by space
+                record_goal(goal, request)
             response.delete_cookie('experiments_goal')
         return response
