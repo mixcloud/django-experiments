57f7a55da1e62856add87e038eb5b72045db3375
Theo Spears <theo@theos.me.uk>
2013-03-08 13:37:36 -0500

    Fix changing between timezone and non-timezone

--- experiments/dateutils.py
+++ experiments/dateutils.py
@@ -6,3 +6,13 @@ if USE_TZ:
 else:
     from datetime import datetime
     now = datetime.now
+
+def fix_awareness(value):
+    tz_aware_value = value.tzinfo is not None and value.tzinfo.utcoffset(value) is not None
+    if USE_TZ and not tz_aware_value:
+        from django.utils.timezone import get_current_timezone
+        return value.replace(tzinfo=get_current_timezone())
+    elif not USE_TZ and tz_aware_value:
+        return value.replace(tzinfo=None)
+    else:
+        return value
