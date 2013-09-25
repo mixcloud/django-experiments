2866808edaefe2cd89954d70f3d9c7d117bd452b
Roberto Barreda <roberto.barreda@gmail.com>
2013-09-25 20:51:20 +0200

    updated setup with branch version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
+import re
 from distutils.core import setup
 from setuptools import find_packages
-import re
 
 # http://bugs.python.org/issue15881
 try:
@@ -23,6 +23,7 @@ def parse_requirements(file_name):
 
     return requirements
 
+
 def parse_dependency_links(file_name):
     dependency_links = []
     for line in open(file_name, 'r').read().split('\n'):
@@ -32,17 +33,18 @@ def parse_dependency_links(file_name):
     return dependency_links
 
 
-setup(name='django-experiments',
-      version='0.3.5',
-      description='Python Django AB Testing Framework',
-      author='Chris Villa',
-      author_email='chris@mixcloud.com',
-      url='https://github.com/mixcloud/django-experiments',
-      packages=find_packages(exclude=["example_project"]),
-      include_package_data=True,
-      license="MIT license, see LICENSE file",
-      install_requires = parse_requirements('requirements.txt'),
-      dependency_links = parse_dependency_links('requirements.txt'),
-      long_description=open('README.rst').read(),
-      test_suite="tests.runtests",
+setup(
+    name='django-experiments',
+    version='0.0.1',
+    description='Python Django AB Testing Framework',
+    author='Roberto Barreda',
+    author_email='roberto.barreda@gmail.com',
+    url='https://github.com/robertobarreda/django-experiments',
+    packages=find_packages(exclude=["example_project"]),
+    include_package_data=True,
+    license="MIT license, see LICENSE file",
+    install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
+    long_description=open('README.rst').read(),
+    test_suite="tests.runtests",
 )
