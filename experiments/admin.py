7fcd834454bc1e992b866ea6764d98810a6ad747
Roberto Barreda <roberto.barreda@gmail.com>
2013-09-25 21:03:56 +0200

    added Enrollment to django admin

--- experiments/admin.py
+++ experiments/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from django import forms
 from experiments.models import Experiment, Enrollment
+
 
 class ExperimentAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date', 'state')
     list_filter = ('name', 'start_date', 'state')
     search_fields = ('name', )
 
+admin.site.register(Enrollment)
 admin.site.register(Experiment, ExperimentAdmin)
