af60d42e891c0a5f5a8060affad9f0123e614ab4
Roberto Barreda <roberto.barreda@gmail.com>
2013-09-30 23:40:43 +0200

    moved admin url rule to the top

--- example_project/urls.py
+++ example_project/urls.py
@@ -5,9 +5,9 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'experiments/', include('experiments.urls')),
     url(r'^$', TemplateView.as_view(template_name="test_page.html"), name="test_page"),
     url(r'^goal/$', TemplateView.as_view(template_name="goal.html"), name="goal"),
-    url(r'^admin/', include(admin.site.urls)),
 )
 
