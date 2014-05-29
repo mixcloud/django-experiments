6f6434652ff9c76dd300bc3d6d94268fcbfe5ce9
Theo Spears <github@theos.me.uk>
2014-05-29 15:05:52 +0100

    Make redis an instance variable
    This means connections will be restablished between requests

--- experiments/counters.py
+++ experiments/counters.py
@@ -8,13 +8,14 @@ REDIS_PORT = getattr(settings, 'EXPERIMENTS_REDIS_PORT', 6379)
 REDIS_PASSWORD = getattr(settings, 'EXPERIMENTS_REDIS_PASSWORD', None)
 REDIS_EXPERIMENTS_DB = getattr(settings, 'EXPERIMENTS_REDIS_DB', 0)
 
-r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=REDIS_EXPERIMENTS_DB)
-
 COUNTER_CACHE_KEY = 'experiments:participants:%s'
 COUNTER_FREQ_CACHE_KEY = 'experiments:freq:%s'
 
 
 class Counters(object):
+    def __init__(self):
+        self.redis = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=REDIS_EXPERIMENTS_DB)
+
     def increment(self, key, participant_identifier, count=1):
         if count == 0:
             return
@@ -22,12 +23,12 @@ class Counters(object):
         try:
             cache_key = COUNTER_CACHE_KEY % key
             freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-            new_value = r.hincrby(cache_key, participant_identifier, count)
+            new_value = self.redis.hincrby(cache_key, participant_identifier, count)
 
             # Maintain histogram of per-user counts
             if new_value > count:
-                r.hincrby(freq_cache_key, new_value - count, -1)
-            r.hincrby(freq_cache_key, new_value, 1)
+                self.redis.hincrby(freq_cache_key, new_value - count, -1)
+            self.redis.hincrby(freq_cache_key, new_value, 1)
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
             pass
@@ -36,12 +37,12 @@ class Counters(object):
         try:
             # Remove the direct entry
             cache_key = COUNTER_CACHE_KEY % key
-            pipe = r.pipeline()
+            pipe = self.redis.pipeline()
             freq, _ = pipe.hget(cache_key, participant_identifier).hdel(cache_key, participant_identifier).execute()
 
             # Remove from the histogram
             freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-            r.hincrby(freq_cache_key, freq, -1)
+            self.redis.hincrby(freq_cache_key, freq, -1)
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
             pass
@@ -49,7 +50,7 @@ class Counters(object):
     def get(self, key):
         try:
             cache_key = COUNTER_CACHE_KEY % key
-            return r.hlen(cache_key)
+            return self.redis.hlen(cache_key)
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
             return 0
@@ -57,7 +58,7 @@ class Counters(object):
     def get_frequency(self, key, participant_identifier):
         try:
             cache_key = COUNTER_CACHE_KEY % key
-            freq = r.hget(cache_key, participant_identifier)
+            freq = self.redis.hget(cache_key, participant_identifier)
             return int(freq) if freq else 0
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
@@ -70,7 +71,7 @@ class Counters(object):
             # briefly be a negative result for some frequency count. We discard these
             # as they shouldn't really affect the result, and they are about to become
             # zero anyway.
-            return dict((int(k), int(v)) for (k, v) in r.hgetall(freq_cache_key).items() if int(v) > 0)
+            return dict((int(k), int(v)) for (k, v) in self.redis.hgetall(freq_cache_key).items() if int(v) > 0)
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
             return tuple()
@@ -78,9 +79,9 @@ class Counters(object):
     def reset(self, key):
         try:
             cache_key = COUNTER_CACHE_KEY % key
-            r.delete(cache_key)
+            self.redis.delete(cache_key)
             freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-            r.delete(freq_cache_key)
+            self.redis.delete(freq_cache_key)
             return True
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
@@ -90,11 +91,11 @@ class Counters(object):
         #similar to above, but can pass pattern as arg instead
         try:
             cache_key = COUNTER_CACHE_KEY % pattern_key
-            for key in r.keys(cache_key):
-                r.delete(key)
+            for key in self.redis.keys(cache_key):
+                self.redis.delete(key)
             freq_cache_key = COUNTER_FREQ_CACHE_KEY % pattern_key
-            for key in r.keys(freq_cache_key):
-                r.delete(key)
+            for key in self.redis.keys(freq_cache_key):
+                self.redis.delete(key)
             return True
         except (ConnectionError, ResponseError):
             # Handle Redis failures gracefully
