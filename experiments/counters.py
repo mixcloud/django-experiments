a4ca2637e47306125c3e82e9225608fcfb23a7ff
Theo Spears <github@theos.me.uk>
2014-05-29 15:04:54 +0100

    Counters to class not just module

--- experiments/counters.py
+++ experiments/counters.py
@@ -14,93 +14,88 @@ COUNTER_CACHE_KEY = 'experiments:participants:%s'
 COUNTER_FREQ_CACHE_KEY = 'experiments:freq:%s'
 
 
-def increment(key, participant_identifier, count=1):
-    if count == 0:
-        return
+class Counters(object):
+    def increment(self, key, participant_identifier, count=1):
+        if count == 0:
+            return
 
-    try:
-        cache_key = COUNTER_CACHE_KEY % key
-        freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-        new_value = r.hincrby(cache_key, participant_identifier, count)
+        try:
+            cache_key = COUNTER_CACHE_KEY % key
+            freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
+            new_value = r.hincrby(cache_key, participant_identifier, count)
 
-        # Maintain histogram of per-user counts
-        if new_value > count:
-            r.hincrby(freq_cache_key, new_value - count, -1)
-        r.hincrby(freq_cache_key, new_value, 1)
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        pass
+            # Maintain histogram of per-user counts
+            if new_value > count:
+                r.hincrby(freq_cache_key, new_value - count, -1)
+            r.hincrby(freq_cache_key, new_value, 1)
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            pass
 
+    def clear(self, key, participant_identifier):
+        try:
+            # Remove the direct entry
+            cache_key = COUNTER_CACHE_KEY % key
+            pipe = r.pipeline()
+            freq, _ = pipe.hget(cache_key, participant_identifier).hdel(cache_key, participant_identifier).execute()
 
-def clear(key, participant_identifier):
-    try:
-        # Remove the direct entry
-        cache_key = COUNTER_CACHE_KEY % key
-        pipe = r.pipeline()
-        freq, _ = pipe.hget(cache_key, participant_identifier).hdel(cache_key, participant_identifier).execute()
+            # Remove from the histogram
+            freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
+            r.hincrby(freq_cache_key, freq, -1)
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            pass
 
-        # Remove from the histogram
-        freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-        r.hincrby(freq_cache_key, freq, -1)
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        pass
+    def get(self, key):
+        try:
+            cache_key = COUNTER_CACHE_KEY % key
+            return r.hlen(cache_key)
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            return 0
 
+    def get_frequency(self, key, participant_identifier):
+        try:
+            cache_key = COUNTER_CACHE_KEY % key
+            freq = r.hget(cache_key, participant_identifier)
+            return int(freq) if freq else 0
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            return 0
 
-def get(key):
-    try:
-        cache_key = COUNTER_CACHE_KEY % key
-        return r.hlen(cache_key)
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        return 0
+    def get_frequencies(self, key):
+        try:
+            freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
+            # In some cases when there are concurrent updates going on, there can
+            # briefly be a negative result for some frequency count. We discard these
+            # as they shouldn't really affect the result, and they are about to become
+            # zero anyway.
+            return dict((int(k), int(v)) for (k, v) in r.hgetall(freq_cache_key).items() if int(v) > 0)
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            return tuple()
 
+    def reset(self, key):
+        try:
+            cache_key = COUNTER_CACHE_KEY % key
+            r.delete(cache_key)
+            freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
+            r.delete(freq_cache_key)
+            return True
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            return False
 
-def get_frequency(key, participant_identifier):
-    try:
-        cache_key = COUNTER_CACHE_KEY % key
-        freq = r.hget(cache_key, participant_identifier)
-        return int(freq) if freq else 0
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        return 0
-
-
-def get_frequencies(key):
-    try:
-        freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-        # In some cases when there are concurrent updates going on, there can
-        # briefly be a negative result for some frequency count. We discard these
-        # as they shouldn't really affect the result, and they are about to become
-        # zero anyway.
-        return dict((int(k), int(v)) for (k, v) in r.hgetall(freq_cache_key).items() if int(v) > 0)
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        return tuple()
-
-
-def reset(key):
-    try:
-        cache_key = COUNTER_CACHE_KEY % key
-        r.delete(cache_key)
-        freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
-        r.delete(freq_cache_key)
-        return True
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        return False
-
-
-def reset_pattern(pattern_key):
-    #similar to above, but can pass pattern as arg instead
-    try:
-        cache_key = COUNTER_CACHE_KEY % pattern_key
-        for key in r.keys(cache_key):
-            r.delete(key)
-        freq_cache_key = COUNTER_FREQ_CACHE_KEY % pattern_key
-        for key in r.keys(freq_cache_key):
-            r.delete(key)
-        return True
-    except (ConnectionError, ResponseError):
-        # Handle Redis failures gracefully
-        return False
+    def reset_pattern(self, pattern_key):
+        #similar to above, but can pass pattern as arg instead
+        try:
+            cache_key = COUNTER_CACHE_KEY % pattern_key
+            for key in r.keys(cache_key):
+                r.delete(key)
+            freq_cache_key = COUNTER_FREQ_CACHE_KEY % pattern_key
+            for key in r.keys(freq_cache_key):
+                r.delete(key)
+            return True
+        except (ConnectionError, ResponseError):
+            # Handle Redis failures gracefully
+            return False
