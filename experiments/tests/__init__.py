1807d2d5f5753347ffdd7873b8aa20666a289603
Mat Clayton <mat@wakari.co.uk>
2014-09-11 21:57:34 +0100

    tidy up requirements

--- experiments/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# from __future__ import absolute_import
-#
-#
-# from . import webuser_incorporate
-#
-#
-# def load_tests(*args, **kwargs):
-#     return webuser_incorporate.load_tests(*args, **kwargs)
