42237b842effe59cbaaa3b3f7db99f83047d6d65
Lizzie Day <ead33@cam.ac.uk>
2014-09-16 15:14:07 +0100

    fix up appconfig

--- experiments/apps.py
+++ experiments/apps.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 
 class ExperimentsConfig(AppConfig):
+    name = 'experiments'
+    label = 'experiments'
+
     def ready(self):
         from django.contrib.auth.signals import user_logged_in
         from experiments.signal_handlers import transfer_enrollments_to_user
