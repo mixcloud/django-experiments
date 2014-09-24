a37cacb96c4021fcc2f9e23e024d8947bb4e644f
Mat Clayton <mat@wakari.co.uk>
2014-09-24 11:23:51 +0100

    tidy up ajax page loads so they count towards experiments

--- experiments/middleware.py
+++ experiments/middleware.py
@@ -2,9 +2,8 @@ from experiments.utils import participant
 
 class ExperimentsRetentionMiddleware(object):
     def process_response(self, request, response):
-        # Don't track, failed pages, ajax requests, logged out users or widget impressions.
-        # We detect widgets by relying on the fact that they are flagged as being embedable
-        if response.status_code != 200 or request.is_ajax() or getattr(response, 'xframe_options_exempt', False):
+        #  We detect widgets by relying on the fact that they are flagged as being embedable, and don't include these in visit tracking
+        if getattr(response, 'xframe_options_exempt', False):
             return response
 
         experiment_user = participant(request)
