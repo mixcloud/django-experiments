15b1b754e01044723a159524e9ca5e7a618c144f
Tyson Clugg <tyson@clugg.net>
2017-05-16 14:57:41 +1000

    Make setup.py conform to common expectations.
    * Separate `install_requires` and `tests_require`
    * Add trove classifiers including Python and Django supported versions
    * Fix license name
    * Dropped workaround for an ancient Python bug which doesn't apply anyway (not using multiprocessing or atexit)

--- setup.py
+++ setup.py
@@ -1,49 +1,48 @@
-from distutils.core import setup
-from setuptools import find_packages
-import re
-
-# http://bugs.python.org/issue15881
-try:
-    import multiprocessing
-except ImportError:
-    pass
+import codecs  # to use a consistent encoding
+from os import path
+from setuptools import setup, find_packages  # prefer setuptools over distutils
 
 
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
+# Get the long description from the README file
+PATH = path.abspath(path.dirname(__file__))
+with codecs.open(path.join(PATH, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
-    return requirements
-
-
-def parse_dependency_links(file_name):
-    dependency_links = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'\s*-[ef]\s+', line):
-            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
-
-    return dependency_links
-
-
-setup(name='django-experiments',
-      version='1.2.0',
-      description='Python Django AB Testing Framework',
-      author='Mixcloud',
-      author_email='technical@mixcloud.com',
-      url='https://github.com/mixcloud/django-experiments',
-      packages=find_packages(exclude=["example_project"]),
-      include_package_data=True,
-      license="MIT license, see LICENSE file",
-      install_requires=parse_requirements('requirements.txt'),
-      dependency_links=parse_dependency_links('requirements.txt'),
-      long_description=open('README.rst').read(),
-      test_suite="testrunner.runtests",
+setup(
+    name='django-experiments',
+    version='1.2.0',
+    description='Python Django AB Testing Framework',
+    long_description=LONG_DESCRIPTION,
+    author='Mixcloud',
+    author_email='technical@mixcloud.com',
+    url='https://github.com/mixcloud/django-experiments',
+    packages=find_packages(exclude=["example_project"]),
+    include_package_data=True,
+    license='MIT',
+    install_requires=[
+        'django>=1.7.0',
+        'django-modeldict-yplan>=1.5.0',
+        'jsonfield>=1.0.3',
+        'redis>=2.4.9',
+    ],
+    tests_require=[
+        'mock>=1.0.1',
+        'tox>=2.3.1',
+    ],
+    test_suite="testrunner.runtests",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+    ],
 )
