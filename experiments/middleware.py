90824779e02f3d70200ee12d139ea4adef642564
Mat Clayton <mat@wakari.co.uk>
2017-05-16 18:51:50 +0100

    add support for django 1.10 middlewares

--- experiments/middleware.py
+++ experiments/middleware.py
@@ -1,7 +1,14 @@
 from experiments.utils import participant
 
+try:
+    # for Django >= 1.10
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    # for Django < 1.10
+    MiddlewareMixin = object
 
-class ExperimentsRetentionMiddleware(object):
+
+class ExperimentsRetentionMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # Don't track, failed pages, ajax requests, logged out users or widget impressions.
         # We detect widgets by relying on the fact that they are flagged as being embedable
