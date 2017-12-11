aff4f9819e2c1d9468261a0401bfe3c381b5a024
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2017-12-11 23:17:02 +0100

    PUMA-105 misc cleanup

--- experiments/conf.py
+++ experiments/conf.py
@@ -26,8 +26,3 @@ CONFIRM_HUMAN_SESSION_KEY = getattr(settings, 'EXPERIMENTS_CONFIRM_HUMAN_SESSION
 DISABLED_EXPERIMENTS_SESSION_KEY = getattr(settings, 'EXPERIMENTS_DISABLED_EXPERIMENTS_SESSION_KEY', 'experiments_disabled')
 
 BOT_REGEX = re.compile("(Baidu|Gigabot|Googlebot|YandexBot|AhrefsBot|TVersity|libwww-perl|Yeti|lwp-trivial|msnbot|bingbot|facebookexternalhit|Twitterbot|Twitmunin|SiteUptime|TwitterFeed|Slurp|WordPress|ZIBB|ZyBorg)", re.IGNORECASE)
-
-
-CONTEXT_VARS = {
-    'user': 'user',
-}
