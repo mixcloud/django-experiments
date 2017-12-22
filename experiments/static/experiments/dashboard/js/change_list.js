a16ee4a1df2fee30293de1b468487a8da6a822a6
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2017-12-22 01:04:03 +0100

    RED-10 state toggles

--- experiments/static/experiments/dashboard/js/change_list.js
+++ experiments/static/experiments/dashboard/js/change_list.js
@@ -10,30 +10,42 @@
             event.preventDefault();
 
             var $this = $(this);
+            if ($this.hasClass('working')) return;
+            var $siblings = $this.parent().children();
             var experimentID = $this.data('id');
             var state = $this.data('state');
             var url = URL_TEMPLATE.replace('EXPERIMENT_ID', experimentID);
+            $siblings.addClass('working');
             $.ajax(
                 url,
                 {
                     method: 'PATCH',
                     contentType: 'application/json',
+                    timeout: 3000,  // milliseconds
                     data: JSON.stringify({state: state})
                 }
             )
-                .done(function() {
-                    console.info( "success" );
-                })
-                .fail(function() {
-                    console.info( "error" );
-                })
-                .always(function() {
-                    console.info( "complete" );
-                });
+            .done(function(data) {
+                var newState = data.state;
+                console.info(newState, $this.data('state'));
+                if (newState !== $this.data('state')) {
+                    $siblings.addClass('error');
+                } else {
+                    $siblings.removeClass('error');
+                }
+                $siblings.removeClass('active');
+                $siblings.filter('[data-state="' + newState + '"]').addClass('active');
+            })
+            .fail(function() {
+                $siblings.removeClass('active');
+                $this.parent().children().addClass('error');
+            })
+            .always(function() {
+                $siblings.removeClass('working');
+            });
         };
 
         $a.on('click', stateToggleClickHandler);
-
 
     });
 
