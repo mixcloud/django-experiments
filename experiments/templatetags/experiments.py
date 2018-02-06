9253f8481b8a8e84958dca6f228c216ecfff5946
Hat Dao <enjoy3013@gmail.com>
2018-02-06 13:52:11 +0700

    Replace urlresolvers with django.urls.resolvers

--- experiments/templatetags/experiments.py
+++ experiments/templatetags/experiments.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from django import template
-from django.core.urlresolvers import reverse
+from django.urls import resolvers as reverse
 
 from experiments.utils import participant
 from experiments.manager import experiment_manager
